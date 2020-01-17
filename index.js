dfc6790f743cbe98ba30403a52c9133fbba996a6
Matt Wood <matt@cratebind.com>
2020-01-17 14:04:34 -0600

    Add eslint-plugin-unicorn, update rules

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ module.exports = {
       jsx: true,
     },
   },
-  plugins: ['prettier', 'import', 'jsx-a11y', 'react', 'react-hooks'],
+  plugins: [
+    'prettier',
+    'import',
+    'jsx-a11y',
+    'react',
+    'react-hooks',
+    'unicorn',
+  ],
   env: {
     browser: true,
     commonjs: true,
@@ -181,7 +188,7 @@ module.exports = {
     ],
     'no-unused-labels': 'warn',
     'no-unused-vars': [
-      'warn',
+      'error',
       {
         args: 'none',
         ignoreRestSiblings: true,
@@ -292,6 +299,42 @@ module.exports = {
 
     // https://github.com/facebook/react/tree/master/packages/eslint-plugin-react-hooks
     'react-hooks/rules-of-hooks': 'error',
+
+    'unicorn/custom-error-definition': 'error',
+    'unicorn/error-message': 'error',
+    'unicorn/escape-case': 'error',
+    'unicorn/explicit-length-check': 'error',
+    'unicorn/filename-case': 0,
+    'unicorn/import-index': 'error',
+    'unicorn/new-for-builtins': 'error',
+    'unicorn/no-array-instanceof': 'error',
+    'unicorn/no-console-spaces': 0,
+    'unicorn/no-fn-reference-in-iterator': 'error',
+    'unicorn/no-for-loop': 'error',
+    'unicorn/no-hex-escape': 'error',
+    'unicorn/no-keyword-prefix': 'off',
+    'unicorn/no-new-buffer': 'error',
+    'unicorn/no-process-exit': 'error',
+    'unicorn/no-unreadable-array-destructuring': 'error',
+    'unicorn/no-unsafe-regex': 'error',
+    'unicorn/no-unused-properties': 'error',
+    'unicorn/no-zero-fractions': 'error',
+    'unicorn/number-literal-case': 'error',
+    'unicorn/prefer-add-event-listener': 'error',
+    'unicorn/prefer-event-key': 'error',
+    'unicorn/prefer-exponentiation-operator': 'error',
+    'unicorn/prefer-flat-map': 'error',
+    'unicorn/prefer-includes': 'error',
+    'unicorn/prefer-node-append': 'error',
+    'unicorn/prefer-node-remove': 'error',
+    'unicorn/prefer-query-selector': 'error',
+    'unicorn/prefer-spread': 'error',
+    'unicorn/prefer-starts-ends-with': 'error',
+    'unicorn/prefer-text-content': 'error',
+    'unicorn/prefer-type-error': 'error',
+    'unicorn/prevent-abbreviations': 0,
+    'unicorn/regex-shorthand': 'error',
+    'unicorn/throw-new-error': 'error',
 
     // prettier-specific rules
     'prettier/prettier': [
